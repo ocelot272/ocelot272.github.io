943cea45a7684be9f06b2f508e2cf10bf04713c7
ocelot272 <luibatuigas@outlook.com>
2026-03-03 11:00:12 -0600

    demo array

---
+++ 04-ball-array/sketch.js
@@ -0,0 +1,40 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  for (let ball of ballArray) {
+    //move
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //display
+    circle(ball.x, ball.y, ball.radius*2);
+  }
+}
+
+function mousePressed() {
+  spawnBall();
+}
+
+function spawnBall() {
+  let someBall = {
+    x: random(width),
+    y: random(height),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    radius: random(10, 30),
+  };
+  ballArray.push(someBall);
+}
