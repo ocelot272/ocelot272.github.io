6995b1a137c0ee20249aac6cd4bbd818f85f9abc
ocelot272 <luibatuigas@outlook.com>
2026-03-03 09:59:11 -0600

    clean up

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,56 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let x;
-let y;
-let speed = 5;
-let psize = 25;
-let fx;
-let fy;
 
 function setup() {
   createCanvas(400, 400);
   noStroke();
   
-  x = width/2;
-  y = height/2;
-  fx = random(0, width);
 }
 
 function draw() {
   background(220);
   
-  drawRec();
-  move();
-  b();
+
 }
 
-function drawRec() {
-  fill("red");
-  rect(x, y, psize, 50);
-}
-
-function move() {
-  if (keyIsDown(68) && x !== width-psize) { // go right
-    x += speed;
-  }
-  if (keyIsDown(65) && x > 0) { // go left
-    x -= speed;
-  }
-}
-
-function b() {
-  fy = bmove();
-  square(fx, fy, 20);
-}
-
-function bmove() {
-  if (fy !== y && fy !== x) {
-    fy++;
-  }
-  return fy;
-}
-
-function th() {
-  
-}
